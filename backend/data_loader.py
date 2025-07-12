691276b21c29479b6977598eb7e7562c8281c971
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-12 12:53:36 +1000

    Update data_loader.py

--- backend/data_loader.py
+++ backend/data_loader.py
@@ -1,1 +1,12 @@
+# -------------------- backend/data_loader.py --------------------
+import pandas as pd
 
+def load_passage_data(file):
+    if file.name.endswith('.xlsx'):
+        return pd.read_excel(file)
+    return pd.read_csv(file)
+
+def load_transaction_data(file):
+    if file.name.endswith('.xlsx'):
+        return pd.read_excel(file)
+    return pd.read_csv(file)
