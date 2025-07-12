72730a29e5cc48a2f41e755b5f3ea4f56898880d
Akhilesh singh <akhi.singh1989@gmail.com>
2025-07-12 12:54:15 +1000

    Update analyser.py

--- backend/analyser.py
+++ backend/analyser.py
@@ -1,1 +1,8 @@
-
+# -------------------- backend/analyser.py --------------------
+def analyse_passage_quality(unmatched_df):
+    issues = {}
+    no_front_lpn = unmatched_df['Front LPN'].isnull().sum()
+    no_tag = unmatched_df['Transaction 1 ID'].isnull().sum()
+    issues['Missing Front LPN'] = no_front_lpn
+    issues['Missing Tag (Transaction ID)'] = no_tag
+    return issues
